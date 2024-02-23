9f9c4d7f0f9f19ba6a39f2868ace59979b2ada4c
sheila <sheila.feijoo@iessanmamede.com>
2024-02-23 09:50:53 +0100

    Muestra botones y el nombre de la puntuación

--- juegos.js
+++ juegos.js
@@ -1,7 +1,7 @@
 const requestOptions = {
     method: "GET",
     redirect: "follow"
-  };
+    };
   
   fetch("http://localhost:8080/api/juego", requestOptions)
     .then((response) => response.json())
@@ -10,11 +10,32 @@ const requestOptions = {
 
 
     function generarBotones(resultados){
-        padre = document.getElementById("Botones");
+        padre = document.getElementById("botones");
 
-        resultados.array.forEach(element => {
+        resultados.forEach( element => { 
             boton=document.createElement("Button");
-            tituloBoton=document.createAttribute("title");
-            tituloBoton.value=element.nombre;
+            boton.innerHTML=element.nombre;
+            boton.addEventListener("click", function () {
+                generarPuntuaciones(element.id);
+              });
+            padre.appendChild(boton);
         });
+    }
+
+    function generarPuntuaciones(id){
+        fetch(`http://localhost:8080/api/puntuacion/juego/${id}`, requestOptions)
+        .then((response) => response.json())
+        .then((result) => generarTabla(result))
+        .catch((error) => console.error(error));
+    }
+
+    function generarTabla(resultados){
+        padre.innerHTML="";
+
+        resultados.forEach(element => {
+            nombre = document.createElement("h6");
+            nombre.innerHTML=element.nombre;
+            padre.appendChild(nombre);
+        })
+
     }
